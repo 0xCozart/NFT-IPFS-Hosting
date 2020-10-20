391db888bfeeb91d72f802d5953c7cbde73a70f6
alan <alanvega002@gmail.com>
2020-10-19 21:34:43 -0700

    created contract event + emitter for uploadImage

--- src/contracts/Tgthr.sol
+++ src/contracts/Tgthr.sol
@@ -4,6 +4,8 @@ contract Tgthr {
     string public name = "Tgthr";
 
     // Store Images
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
     struct Image {
         uint256 id;
         string hash;
@@ -12,12 +14,29 @@ contract Tgthr {
         address payable author;
     }
 
-    mapping(uint256 => Image) public images;
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
 
     // Create Images
-    function uploadImage() public {
-        images[1] = Image(1, "abc123", "Hello World!", 0, address(0x0));
-    }
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        imageCount = imageCount++;
 
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
     // Tip Images
 }
