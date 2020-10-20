a1cc625fcbd447101d7308cf21c8ae0b9072709e
alan <alanvega002@gmail.com>
2020-10-19 23:58:26 -0700

    tipImageOwner tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+const Web3 = require("web3");
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
 /* eslint-disable no-undef */
 const Tgthr = artifacts.require("./Tgthr.sol");
 
@@ -41,11 +44,58 @@ contract("Tgthr", ([deployer, author, tipper]) => {
       const event = result.logs[0].args
 
       assert.equal(imageCount, 1);
-      assert.equal(event.hash, hash, 'hash is correct');
-      assert.equal(event.description, 'Image description', 'Description id correct');
-      assert.equal(event.tipAmount, '0', 'Tip amount is correct');
-      assert.equal(event.author, author, 'author is correct')
+      assert.equal(event.hash, hash, 'Hash is incorrect');
+      assert.equal(event.description, 'Image description', 'Description id incorrect');
+      assert.equal(event.tipAmount, 0, 'Tip amount is incorrect');
+      assert.equal(event.author, author, 'Author is incorrect')
 
+
+      await tgthr.uploadImage("", 'Image description', {from: author}).should.be.rejected;
     });
+
+    it('lists images', async() => {
+      const image = await tgthr.images(imageCount)
+
+      assert.equal(imageCount, 1);
+      assert.equal(image.hash, hash, 'Hash is incorrect');
+      assert.equal(image.description, 'Image description', 'Description id incorrect');
+      assert.equal(image.tipAmount, 0, 'Tip amount is incorrect');
+      assert.equal(image.author, author, 'Author is incorrect')
+
+    })
+
+    it('allows users to tip images', async() => {
+      // Track authors balance before tip
+      let oldAuthorBalance;
+      oldAuthorBalance = await web3.eth.getBalance(author)
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+      result = await tgthr.tipImageOwner(imageCount, {from:tipper, value: web3.utils.toWei("1", "ether")})
+
+      // Success 
+      const event = result.logs[0].args
+
+      assert.equal(imageCount, 1);
+      assert.equal(event.hash, hash, 'Hash is incorrect');
+      assert.equal(event.description, 'Image description', 'Description id incorrect');
+      assert.equal(event.tipAmount, "1000000000000000000", 'Tip amount is incorrect');
+      assert.equal(event.author, author, 'Author is incorrect')
+
+      // Checks that the author received funds
+      let newAuthorBalance;
+      newAuthorBalance = await web3.eth.getBalance(author)
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+      let tipForImageOwner;
+      tipForImageOwner = web3.utils.toWei("1", "Ether")
+      tipForImageOwner = new web3.utils.BN(tipForImageOwner)
+
+      const expectedBalance = oldAuthorBalance.add(tipForImageOwner)
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+      // Failure: Tries to tip a image that does not exist
+      await tgthr.tipImageOwner(99, {from:tipper, value: web3.utils.toWei("1", "ether")}).should.be.rejected;
+    })
   });
 });
