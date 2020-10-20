58ec7600ac83e30de6566db98da1a3876ec6be0c
alan <alanvega002@gmail.com>
2020-10-19 23:57:58 -0700

    tipImageOwner method added

--- src/contracts/Tgthr.sol
+++ src/contracts/Tgthr.sol
@@ -22,10 +22,22 @@ contract Tgthr {
         address payable author
     );
 
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
     // Create Images
     function uploadImage(string memory _imgHash, string memory _description)
         public
     {
+        require(bytes(_description).length > 0, "Description is of length 0");
+        require(bytes(_imgHash).length > 0, "Hash is of length 0");
+        require(msg.sender != address(0x0));
+
         imageCount++;
 
         images[imageCount] = Image(
@@ -38,5 +50,27 @@ contract Tgthr {
 
         emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
     }
+
     // Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= imageCount, "Invalid id");
+
+        Image memory _image = images[_id];
+
+        address payable _author = _image.author;
+
+        address(_author).transfer(msg.value);
+
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        images[_id] = _image;
+
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
